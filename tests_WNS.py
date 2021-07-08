a3fdb063f3b1e6e099d3b30fc9fc183daf86a5b7
Pepe Domenech Gomis <jdomenechgomis@gmail.com>
2021-07-08 22:26:33 +0200

    Commented test file to avoid confusion since it is still not developed.

--- tests_WNS.py
+++ tests_WNS.py
@@ -1,10 +1,10 @@
-import unittest
-from .WNS import *
+# import unittest
+# from .WNS import *
 
-class TestWNS(unittest.TestCase):
+# class TestWNS(unittest.TestCase):
 
-# class TestLemmatize(unittest.TestCase):
+# # class TestLemmatize(unittest.TestCase):
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
